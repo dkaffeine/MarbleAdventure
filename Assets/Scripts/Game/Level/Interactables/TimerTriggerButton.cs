d2a5ca38c8bab2a2a61e269087f01296fd9db914
Adrien Semin <adrien.semin@gmail.com>
2021-04-28 22:44:34 +0200

    Game Engine
    * Tutorial level complete
    * Goal flag's texture modified
    * Trigger functions modified so that they have
    * Fixed new end level flag

--- Assets/Scripts/Game/Level/Interactables/TimerTriggerButton.cs
+++ Assets/Scripts/Game/Level/Interactables/TimerTriggerButton.cs
@@ -18,7 +18,6 @@ public class TimerTriggerButton : InteractableElement
     // Start is called before the first frame update
     void Start()
     {
-        volumeSE = gameObject.GetComponent<VolumeSE>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Game/Level/Interactables/TriggerButton.cs
+++ Assets/Scripts/Game/Level/Interactables/TriggerButton.cs
@@ -16,7 +16,6 @@ public class TriggerButton : InteractableElement
     // Start is called before the first frame update
     void Start()
     {
-        volumeSE = gameObject.GetComponent<VolumeSE>();
     }
 
     // Update is called once per frame
